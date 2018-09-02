76b97c9de626739bbaa8b0d602df46eacec98690
camilocls <camilocls@users.noreply.github.com>
2018-09-02 12:28:11 -0500

    Updated Token

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,3 @@
-export const TOKEN = '8bed3b5571567436f2b2b46aed0e45c8959d6923';
+// This is only for development purposes.
+// This token is going to be eliminated in few days.
+export const TOKEN = '9ae3894842b8a01068750030f0b1b575952e5a84';
