0f8bd8faeda7a5b404f7a8b5abf8f0fd6077565b
camilocls <camilocls@users.noreply.github.com>
2018-11-07 01:55:14 -0500

    - Remove access token from repository. - The access token has been updated.

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 // This is only for development purposes.
 // This token is going to be eliminated in few days.
-export const TOKEN = '9ae3894842b8a01068750030f0b1b575952e5a84';
+export const TOKEN = process.env.GITHUB_TOKEN;
